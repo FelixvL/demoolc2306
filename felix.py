866314c3e469ec2f1c5496b0df73c9813601f073
Lon Lrn <lonlrn@android-2aab2749e3193eee.2e.lan>
2023-06-20 15:37:40 +0200

    go

--- felix.py
+++ felix.py
@@ -1,2 +1,13 @@
+import mysql.connector
+
 def mijnownfunction():
+  mysql.connector.connect(
+    host="localhost",
+    port="8889",
+    username="root",
+    password="root",
+    database="olcdb2306"
+  )
   return "this is from felix"
+
+mijnownfunction()
